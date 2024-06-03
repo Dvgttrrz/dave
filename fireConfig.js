a29d0a534690a42acf6718f92590f21839723efc
Dvgttrrz <165793374+Dvgttrrz@users.noreply.github.com>
2024-06-03 09:58:45 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyD3zMPB3AIc0_IVV8Shq-2wKCHvVEHw_OE",
+  authDomain: "davemain2.firebaseapp.com",
+  databaseURL: "https://davemain2-default-rtdb.firebaseio.com",
+  projectId: "davemain2",
+  storageBucket: "davemain2.appspot.com",
+  messagingSenderId: "624472869580",
+  appId: "1:624472869580:web:b6065801fef55e404cd069"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
